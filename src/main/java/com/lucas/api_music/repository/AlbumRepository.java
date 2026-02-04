ec62aafc55ad5a125dffa9224b52a3d99a552cd9
lucas <informatica17@martinello.com.br>
2026-02-04 14:49:48 -0400

    feat: Adicionada a classe AlbumRepository no pacote repository.

---
+++ src/main/java/com/lucas/api_music/repository/AlbumRepository.java
@@ -0,0 +1,72 @@
+package com.lucas.api_music.repository;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.lucas.api_music.model.entity.Album;
+import com.lucas.api_music.model.entity.Artista;
+
+public interface AlbumRepository extends JpaRepository<Album, Long> {
+
+    @Override
+    Page<Album> findAll(Pageable pageable);
+
+    List<Album> findByArtista(Artista artista);
+
+    Page<Album> findByArtistaId(Long artistaId, Pageable pageable);
+
+    Page<Album> findByNomeContainingIgnoreCase(
+            String name,
+            Pageable pageable
+    );
+
+    @Query("""
+        SELECT a FROM Album a
+        JOIN a.artista ar
+        WHERE LOWER(ar.nome) LIKE LOWER(CONCAT('%', :name, '%'))
+    """)
+    Page<Album> findByArtistaName(
+            @Param("nome") String nome,
+            Pageable pageable
+    );
+
+    @Query("""
+        SELECT a FROM Album a
+        JOIN a.artista ar
+        WHERE ar.tipo = :type
+    """)
+    Page<Album> findByArtistaType(
+            @Param("tipo") String tipo,
+            Pageable pageable
+    );
+
+
+    @Query("""
+        SELECT a FROM Album a
+        JOIN a.artista ar
+        WHERE
+            (:artistName IS NULL OR LOWER(ar.nome) LIKE LOWER(CONCAT('%', :artistaNome, '%')))
+        AND
+            (:albumName IS NULL OR LOWER(a.nome) LIKE LOWER(CONCAT('%', :albumNome, '%')))
+        AND
+            (:tipo IS NULL OR ar.tipo = :tipo)
+    """)
+    Page<Album> searchAdvanced(
+            @Param("artistaNome") String artistaNome,
+            @Param("albumNome") String albumNome,
+            @Param("tipo") String tipo,
+            Pageable pageable
+    );
+
+
+    Page<Album> findAllByOrderByNomeAsc(Pageable pageable);
+
+    Page<Album> findAllByOrderByNomeDesc(Pageable pageable);
+
+
+}
