6c1a493d1c9bfe3b58255f1db9289409b916719b
lucas <informatica17@martinello.com.br>
2026-02-04 13:48:35 -0400

    chore: Estrutura do Projeto Spring Boot
    Neste commit inclui:
    
    Spring Initializr;
    Dependências base;
    Estrutura inicial.

---
+++ src/main/java/com/lucas/api_music/ApiMusicApplication.java
@@ -0,0 +1,13 @@
+package com.lucas.api_music;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMusicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucas/api_music/ApiMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas.api_music;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
